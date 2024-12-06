4c1927b68805caff4c2f3550bc8273ff3cbdc4aa
NeesH73 <144137791+NeesH73@users.noreply.github.com>
2024-12-06 12:25:05 +0000

    dz

--- lectures/lecture3-4/matrix.cpp
+++ lectures/lecture3-4/matrix.cpp
@@ -1,58 +1,88 @@
 #include <iostream>
 
-const int mSize = 3;
+using namespace std;
 
-int matrix_sum(int** m) // int**
-{
-    int sum = 0;
-    for (int i = 0; i < mSize; i++) {
-        for (int j = 0; j < mSize; j++) {
-            sum += m[i][j];
-        }
-    }
-    return sum;
-}
-
-
-void matrix_transpose(int** m) // int**
-{
-    for (int i = 0; i < mSize; i++) {
-        for (int j = i + 1; j < mSize; j++) {
-            std::swap(m[i][j], m[j][i]);
-        }
-    }
-}
-
-void matrix_print(int** m) // int**
-{
-    for (int i = 0; i < mSize; i++) {
-        for (int j = 0; j < mSize; j++) {
-            std::cout << m[i][j] << ' ';
-        }
-        std::cout << std::endl;
-    }
-}
-
-
-int main(int argc, char** argv)
-{
-    
-    int** matrix=new int*[mSize];
-    for (int i=0;i<mSize;i++){
-        matrix[i]=new int[mSize];
+int** multMatrix(int** matrixA, int rowsA, int colsA, int** matrixB, int rowsB, int colsB){
+    if (colsA != rowsB) {
+        return nullptr;
     }
     
-    for (int i = 0; i < mSize; i++) {
-        for (int j = 0; j < mSize; j++) {
-            std::cin >> matrix[i][j];
+    int** result = new int*[rowsA];
+    for (int i = 0; i < rowsA; i++) {
+        result[i] = new int[colsB]{0};
+    }
+    
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsB; j++) {
+            for (int k = 0; k < colsA; k++) {
+                result[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
         }
     }
-    int matrixSum = matrix_sum(matrix);
+    return result;
+}
 
-	std::cout << "square matrix sum = " << (matrixSum*matrixSum) << std::endl;
+void delMatrix(int** matrix, int rows) {
+    for (int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
 
-    matrix_transpose(matrix);
-    matrix_print(matrix);
+int main() {
+    int rowsA, colsA, rowsB, colsB;
+    
+    cin >> rowsA >> colsA;
 
-	return 0;
+    int** matrixA = new int*[rowsA];
+    for (int i = 0; i < rowsA; i++) {
+        matrixA[i] = new int[colsA];
+    }
+
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsA; j++) {
+            cin >> matrixA[i][j];
+        }
+    }
+   
+    cin >> rowsB >> colsB;
+
+    int** matrixB = new int*[rowsB];
+    for (int i = 0; i < rowsB; i++) {
+        matrixB[i] = new int[colsB];
+    }
+
+    for (int i = 0; i < rowsB; i++) {
+        for (int j = 0; j < colsB; j++) {
+            cin >> matrixB[i][j];
+        }
+    }
+
+    int** result = multMatrix(matrixA, rowsA, colsA, matrixB, rowsB, colsB);
+
+    
+    if (result == nullptr) {
+        cerr << "Error: Incompatible matrix dimensions." << endl;
+
+        
+        delMatrix(matrixA, rowsA);
+        delMatrix(matrixB, rowsB);
+
+        return 1;
+    }
+
+    
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsB; j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    
+    delMatrix(matrixA, rowsA);
+    delMatrix(matrixB, rowsB);
+    delMatrix(result, rowsA);
+
+    return 0;
 }
